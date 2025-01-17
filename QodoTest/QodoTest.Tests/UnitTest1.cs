11f34aa66574e1a4d2bd86cf9bb3889f756631f4
Jakub Kleberc <jkleberc@vendavo.com>
2025-01-17 16:09:24 +0100

    init commit

---
+++ QodoTest/QodoTest.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace QodoTest.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ QodoTest/QodoTest/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace QodoTest;
+
+public class TestClass
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
+    }
+}
